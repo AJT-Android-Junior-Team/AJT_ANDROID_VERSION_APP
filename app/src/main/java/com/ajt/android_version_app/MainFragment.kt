e6880dd2ce60087482d3e63ab5ddf08dc33cc81e
ncliff-git <moon.light.2000az@gmail.com>
2021-09-16 16:25:20 +0300

    Updated the design

--- app/src/main/java/com/ajt/android_version_app/MainFragment.kt
+++ app/src/main/java/com/ajt/android_version_app/MainFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ajt.android_version_app.databinding.FragmentMainBinding
-import java.lang.RuntimeException
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
